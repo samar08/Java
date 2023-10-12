dd702427872161cfcdc20eeb012c68c0d315886b
samar08 <samarasimhareddy.mohan@gmail.com>
2023-10-12 08:34:55 +0530

    Coinchange2 problem added by samar for Hacktoberfest

---
+++ src/main/java/com/thealgorithms/dynamicprogramming/Coinchange2.java
@@ -0,0 +1,29 @@
+public class Coinchange2 {
+    public int change(int amount, int[] coins) {
+        int n=coins.length;
+        int i,j;
+        int[][] dp=new int[n][amount+1];
+        for(i=0;i<n;i++){
+            for(j=0;j<=amount;j++){
+                if(i==0){
+                    if(j%coins[i]==0){
+                        dp[i][j]=1;
+                    }
+                    else{
+                        dp[i][j]=0;
+                    }
+                }
+                else{
+                    if(coins[i]<=j){
+                        dp[i][j]=dp[i-1][j]+dp[i][j-coins[i]];
+                    }
+                    else{
+                        dp[i][j]=dp[i-1][j];
+                    }
+                    
+                }
+            }
+        }
+        return dp[n-1][amount];
+    } 
+}
